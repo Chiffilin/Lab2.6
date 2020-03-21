912ddf5483aa6911b5f2b18306e4b598de0fc963
Chiffilin <dimaaitbaev42@gmail.com>
2020-03-21 10:48:01 +0200

    Добавьте файлы проекта.

---
+++ Lab2.6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class ArrayExample
+{
+    static void Main()
+    {
+        int L;
+        Console.WriteLine("Введите размер массива:");
+        while (!int.TryParse(Console.ReadLine(), out L))                //проверка на ввод переменных
+        {
+            Console.WriteLine("Ошибка ввода! Введите целое число L");
+        }
+        int[] a = new int[L];
+        for (int i = 0; i < a.Length; i++)
+        {
+            a[i] = i+1;
+        }
+        for (int i = 0; i < a.Length; i++)
+        {
+            Console.WriteLine($"a[{i+1}] = {a[i]}");
+        }
+    }
+}
